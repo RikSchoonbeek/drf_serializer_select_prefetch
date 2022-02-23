c65f2d88a22856f9435ebb77fb76acd1009defe6
rikschoonbeek <rik.schoonbeek@gmail.com>
2022-02-23 10:07:57 +0000

    changed Unit model + migrations

--- organization/models.py
+++ organization/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db import models
 
 
@@ -13,6 +15,14 @@ class Unit(models.Model):
     employees = models.ManyToManyField("Employee", blank=True)
     name = models.CharField(max_length=50)
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+
+    # The GenericForeignKey for demonstration purposes. So the functionality
+    # may seem kind of random
+    content_type = models.ForeignKey(
+        ContentType, on_delete=models.CASCADE, blank=True, null=True
+    )
+    object_id = models.PositiveIntegerField(blank=True, null=True)
+    some_object = GenericForeignKey()
 
     def __str__(self):
         return self.name
